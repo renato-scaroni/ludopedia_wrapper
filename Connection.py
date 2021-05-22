07131753519866a0c5a44f11d9daa2544f5f3faf
scaroni <renato.scaroni@caminoeducation.com>
2021-05-22 15:43:30 -0300

    feat: added authentication and token persistance

--- Connection.py
+++ Connection.py
@@ -1,10 +1,16 @@
 import requests
 import json
 import sys
+from flask import Flask, request
+from multiprocessing import Process
+import webbrowser
+import pickle
+from datetime import datetime
 
 SUCCESS_COLOR = "\033[92m"
 ERROR_COLOR = "\033[91m"
 END_COLOR = "\033[0m"
+TIME_FORMAT = "%y-%m-%d"
 
 
 class MissingConfigError(Exception):
@@ -21,6 +27,14 @@ class MissingConfigError(Exception):
 class Connnection:
     def __init__(self, conf_file = "app_conf.json") -> None:
         self.load_conf(conf_file)
+        try:
+            self.load_token()
+            if (datetime.now() - self.token_reg_date).days >= 60:
+                raise Exception
+            print(f"{SUCCESS_COLOR}Success!{END_COLOR} token info successfully loaded.")
+        except:
+            error = f"{ERROR_COLOR}Error:{END_COLOR} Token not existing or expired. Please run connect method"
+            print(error, file=sys.stderr)
 
     def load_conf(self, conf_file = "app_conf.json"):
         try:
@@ -38,7 +52,38 @@ class Connnection:
         print(f"{SUCCESS_COLOR}Success!{END_COLOR} {conf_file} successfully loaded {self}")
 
     def connect(self):
-        pass
+        self.validate_config()
+        url = f"https://ludopedia.com.br/oauth?app_id={self.APP_ID}&&redirect_uri=http://localhost:5000"
+
+        print("--------------------------------------------")
+        print(f"Please go to this url in your web browser {url}")
+        print("--------------------------------------------")
+        webbrowser.open(url, new=2)
+        app = Flask(__name__)
+        @app.route("/")
+        def hello_world():
+            self.code = request.args['code']
+            return self.authenticate(self.code)
+            # return "Please go back to your application"
+        app.run()
+
+    def authenticate(self, code):
+        url = "https://ludopedia.com.br/tokenrequest"
+        response = requests.post(url, data={"code":code})
+        self.access_token =  json.loads(response.text)["access_token"]
+        self.save_token(self.access_token)
+        return response.__str__()
+
+    def load_token(self):
+        with open('data.pickle', 'rb') as f:
+            access_info = pickle.load(f)
+            self.access_token = access_info['token']
+            self.token_reg_date = datetime.strptime(access_info['token_reg_date'], TIME_FORMAT)
+            return self.access_token, self.token_reg_date
+
+    def save_token(self, access_token):
+        with open('data.pickle', 'wb') as f:
+            pickle.dump({"token": access_token, "token_reg_date": datetime.now().strftime(TIME_FORMAT)}, f, pickle.HIGHEST_PROTOCOL)
 
     def validate_config(self):
         errors = []
@@ -48,10 +93,6 @@ class Connnection:
                 errors.append(f"Configuration {attr} is missing, please add it to your config file and reload the config.")
         if len(errors) > 0:
             raise MissingConfigError("\n".join(errors))
-
-
-            
-
 
     def __str__(self) -> str:
         try:
